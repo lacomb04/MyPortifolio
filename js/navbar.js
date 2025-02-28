64bfa0a808cf5a1c9a2ad86e847a1cc0c46f0fdb
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-02-28 22:58:10 +0000

    deixei o nav scroll para arrumar

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,1 @@
 const navbar = document.querySelector("header nav");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    navbar.classList.toggle("scrolling");
-  } else {
-    navbar.classList.remove("scrolling");
-  }
-});
