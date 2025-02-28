b5bc24e0f6b60740983db3b353ef1de7484cfe3d
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-02-28 14:24:00 +0000

    Banner principal

--- js/dark-light.js
+++ js/dark-light.js
@@ -1,15 +1,21 @@
 let menumobile = document.querySelectorAll("#menu-btn div");
 let trilho = document.getElementById("trilho");
 let body = document.querySelector("body");
-let boldNeon = document.getElementById("boldNeon");
+let boldNeon = document.querySelectorAll(".bold-neon");
+let linkHover = document.querySelectorAll("#navList li");
 let navList = document.getElementById("navList");
 
 trilho.addEventListener("click", () => {
   trilho.classList.toggle("dark");
   body.classList.toggle("dark");
-  boldNeon.classList.toggle("dark");
+  boldNeon.forEach((elemento) => {
+    elemento.classList.toggle("dark");
+  });
   navList.classList.toggle("dark");
   menumobile.forEach((div) => {
     div.classList.toggle("dark");
+  });
+  linkHover.forEach((link) => {
+    link.classList.toggle("dark");
   });
 });
