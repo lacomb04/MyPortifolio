f04af1a9f34251be517cb138dfb47a2e56e10752
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-02-28 19:49:42 +0000

    scrool2

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector("header nav");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 30) {
+  if (window.scrollY > 100) {
     navbar.classList.toggle("scrolling");
   } else {
     navbar.classList.remove("scrolling");
